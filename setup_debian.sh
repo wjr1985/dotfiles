a49e5dc411c9504f4f0d9eb67478c521b8e15c96
Bill Rastello <wjr1985@gmail.com>
2019-04-17 07:39:46 -0500

    rename script

--- setup_debian.sh
+++ setup_debian.sh
@@ -16,7 +16,7 @@ sudo apt-get install htop
 sudo apt-get install vim
 
 # get my vim dotfiles
-mkdir -p ~/github && cd ~/github && git clone git@gitlab.com:wjr1985/vim_dotfiles && cd vim_dotfiles && ./activate.sh
+mkdir -p ~/github && cd ~/github && git clone git@github.com:wjr1985/vim_dotfiles && cd vim_dotfiles && ./activate.sh
 
 # get git-prompt.sh
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -O ~/git-prompt.sh
