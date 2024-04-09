b99210823b2634a9888d302123b48e34164d81ca
Bill Rastello <wjr1985@gmail.com>
2024-04-09 12:46:01 -0500

    add arch setup script

---
+++ setup_arch_zsh.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+# install zsh
+# sudo apt-get install -y zsh
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# install bat
+# wget https://github.com/sharkdp/bat/releases/download/v0.6.0/bat_0.6.0_amd64.deb && sudo dpkg -i bat_0.6.0_amd64.deb && rm bat_0.6.0_amd64.deb
+
+# install prettyping
+# sudo wget -P /usr/bin/ https://raw.githubusercontent.com/denilsonsa/prettyping/master/prettyping && sudo chmod a+x /usr/bin/prettyping
+
+# install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
+
+# install htop
+# sudo apt-get install -y htop
+
+# make sure neovim is installed
+sudo pacman -S neovim
+
+# get my vim dotfiles
+#mkdir -p ~/github && cd ~/github && git clone git@github.com:wjr1985/vim_dotfiles && cd vim_dotfiles && ./activate.sh
+
+# get git-prompt.sh
+# wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -O ~/git-prompt.sh
+
+# get ncdu
+#sudo apt-get install -y ncdu
+sudo pacman -S ncdu
+
+# # get pure shell
+# mkdir ~/.zfunctions
+# cd ~/github && git clone git@github.com:sindresorhus/pure && cd pure && ln -s "$PWD/pure.zsh" "$HOME/.zfunctions/prompt_pure_setup" && ln -s "$PWD/async.zsh" "$HOME/.zfunctions/async"
+#
+# get powerlevel 10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+ln -s $PWD/p10k.zsh ~/.p10k.zsh
+
+# install the zshrc from this repo
+mv ~/.zshrc ~/.zshrc-bak
+cd ~/github/dotfiles
+ln -s $PWD/zshrc ~/.zshrc
